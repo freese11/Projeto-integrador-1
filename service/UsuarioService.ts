cc8b3293e351834a359620c80bd35dd9c05d0dac
freese11 <lucasfreese444@gmail.com>
2025-02-27 19:10:00 -0300

    Add files via upload

---
+++ service/UsuarioService.ts
@@ -0,0 +1,44 @@
+import { Usuario } from "../src/entity/usuario";
+import { UsuarioRepository } from "../src/repository/UsuarioRepository";
+UsuarioRepository
+
+export class UsuarioService {
+
+
+  private repo: UsuarioRepository
+
+  constructor() {
+    this.repo = new UsuarioRepository()
+
+
+  }
+  async listarUsuarios1(): Promise<Usuario[]> {
+    return await this.repo.listarUsuarios1()
+  }
+
+  public async inserirUsuario(nome: string, cpf: string, email: string, numero: string) {
+    if (this.validarEmail(email))
+      return await this.repo.inserirUsuario(nome, cpf, email, numero)
+  }
+  private validarEmail(email: string) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+  public async deletarUsuario(cpf: string): Promise<Usuario[]> {
+    let lista: Usuario[] = [];
+    lista = await this.repo.deletarUsuario(cpf)
+    return lista
+  }
+
+public async retornarUsuario(cpf:string) :Promise<Usuario> {
+  let usuario :Usuario
+  
+  usuario= await this.repo.retornarUsuario(cpf)
+  
+
+return usuario
+  
+  }
+
+
+}
---
+++ src/view/VendasView.ts
@@ -0,0 +1,52 @@
+const prompt = require("prompt-sync")
+import promptSync from "prompt-sync";
+import { VendasService } from "../../service/VendasService";
+
+
+
+export class VendasView {
+    private vendas: VendasService
+    private prompt: promptSync
+
+
+    constructor() {
+        this.vendas = new VendasService
+        this.prompt = promptSync();
+    }
+
+
+
+
+public async esxibirMenu(): Promise<void> {
+    console.log("FreeseSalesSystem")
+    console.log("")
+    console.log("1 - Mostrar vendas")
+    console.log("2 - Cadastrar produto")
+    //console.log("3 - buscar  produto")
+    //console.log("4 - deletar produto")
+    //console.log("5 - sair")
+
+    let pergunta = this.prompt("Selecione alguma das opções acima  ")
+    switch (pergunta) {
+case "1":
+    console.log("lista abaixo")
+    console.table(await this.vendas.listarVendas())
+    this.esxibirMenu()
+    break;
+case "2":
+    console.log("insira as informaçoes abaixo do usuario:")
+                let codV=this.prompt("digite o codigo da venda :")
+                let codP=this.prompt("digite o codigo do Produto: ")
+                let dataa=this.prompt("digite a data:")
+                let quantidad=this.prompt("digite a quantidade :")
+                let valor=this.prompt("digite o valor total :")
+                let codU=this.prompt("digite o codigo do usuario: ")
+                let stats=this.prompt("digite o status da venda:")
+                let codC=this.prompt("digite o codigo do cliente :")
+                await this.vendas.inserirVenda(codV,codP,dataa,quantidad,valor,codU,stats,codC)
+                console.log("vendas adicionado com sucesso")
+                this.esxibirMenu()
+    }
+}
+
+}
