d98a74257467549b4d9c99c94d59aaf8c2d98645
freese11 <lucasfreese444@gmail.com>
2025-02-27 21:18:33 -0300

    Add files via upload

---
+++ src/entity/usuario.ts
@@ -0,0 +1,14 @@
+export class Usuario {
+    private nome: string
+    private cpf: string
+    private email: string
+    private numero: string
+   constructor(nome:string,cpf:string,email:string,numero:string){
+    this.nome=nome
+    this.cpf=cpf
+    this.email=email
+    this.numero=numero
+   }
+
+
+}
